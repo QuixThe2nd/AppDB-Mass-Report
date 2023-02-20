32284c90c12929151661ad9eb9a17838729e54c5
Parsa Yazdani <3parsa3@gmail.com>
2023-02-20 20:04:46 +1100

    Support for non-tvos apps + better error handling

--- index.py
+++ index.py
@@ -24,21 +24,24 @@ def check_link(id, rt, i, type):
         report_link(id, 'HTTP ' + str(code), link, i, type)
         return
 
-    content = requests.get(link).text
-    if 'WILLKOMMEN IM UNTERGRUND!' in content or 'File Not Found' in content:
-        report_link(id, 'File not found', link, i, type)
-    elif 'The file you are trying to download is no longer available' in content:
-        report_link(id, 'File no longer available', link, i, type)
-    elif 'File has expired' in content:
-        report_link(id, 'File has expired', link, i, type)
-    else:
-        if 'bayfiles.com' in link or 'github.com' in link or 'unc0ver.dev' in link or 'userscloud.com' in link:
-            return
-        print('Unknown Status: ' + link)
+    try:
+        content = requests.get(link).text
+        if 'WILLKOMMEN IM UNTERGRUND!' in content or 'File Not Found' in content:
+            report_link(id, 'File not found', link, i, type)
+        elif 'The file you are trying to download is no longer available' in content:
+            report_link(id, 'File no longer available', link, i, type)
+        elif 'File has expired' in content:
+            report_link(id, 'File has expired', link, i, type)
+        else:
+            if 'bayfiles.com' in link or 'github.com' in link or 'unc0ver.dev' in link or 'userscloud.com' in link:
+                return
+            print('Unknown Status: ' + link)
+    except:
+        print('Failed to check status')
 
 
 def report_link(link, reason, url, i, type):
-    print(type + ' ' + str(i) + ': ' + reason + ': ' + requests.get(f'https://api.dbservices.to/v1.5/?action=report&type=tvos&id={link}&reason={reason}&lt={lt}').text + ' - ' + url)
+    print(type + ' ' + str(i) + ': ' + reason + ': ' + requests.get(f'https://api.dbservices.to/v1.5/?action=report&type={type}&id={link}&reason={reason}&lt={lt}').text + ' - ' + url)
 
 
 def call_check_app(app, i, type):
@@ -48,24 +51,34 @@ def call_check_app(app, i, type):
 
 def check_app(app, i, type):
     trackid = app['trackid']
-    # print('Name: ' + app['name'])
-    link_data = requests.get(f'https://api.dbservices.to/v1.5/?action=get_links&type=tvos&trackids={trackid}').json()
+    print('Name: ' + app['name'])
+    link_data = requests.get(f'https://api.dbservices.to/v1.5/?action=get_links&type={type}&trackids={trackid}').json()
 
     if link_data['data'] is None:
         return
 
-    for k, v in link_data['data'].items():
-        for ver, links in v.items():
-            for link in links:
+    for app_id, app in link_data['data'].items():
+        final_app = []
+        try:
+            for ver, links in app.items():
+                final_app.append(links)
+        except:
+            for links in app:
+                final_app.append(links)
 
-                if link['host'] == 'mega.nz' or 'starfiles' in link['host'] or '.onion' in link['host']:
+        for links in final_app:
+            for link in links:
+                if link['host'] is not None and (link['host'] == 'mega.nz' or 'starfiles' in link['host'] or '.onion' in link['host']):
                     continue
 
-                response = requests.get('https://api.dbservices.to/v1.5/?action=process_redirect&t=' + link['link'].replace('ticket://', ''))
-                call_check_link(link['id'], response.json()['data']['redirection_ticket'], i, type)
+                try:
+                    response = requests.get('https://api.dbservices.to/v1.5/?action=process_redirect&t=' + link['link'].replace('ticket://', ''))
+                    call_check_link(link['id'], response.json()['data']['redirection_ticket'], i, type)
+                except:
+                    print('Request Failed')
 
 
-for type in ["ios", "cydia", "osx", "standalone", "tvos", "books"]:
+for type in ["books", "osx", "standalone", "cydia", "ios", "tvos"]:
     print(f'Type: {type}')
     page = 0
     i = 0
@@ -78,5 +91,6 @@ for type in ["ios", "cydia", "osx", "standalone", "tvos", "books"]:
             break
 
         for app in apps:
-            call_check_app(app, i, type)
             i += 1
+            call_check_app(app, i, type)
+            time.sleep(1)
